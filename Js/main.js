af2202c0592249bd552fa0bbdd1aa82a555c8c3b
Simon-M20 <75553043+Simon-M20@users.noreply.github.com>
2021-11-07 18:14:05 -0400

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,11 @@
-import hola from "./module/hola.js";
+import { menuShow } from "./module/menuShow.js";
 import scrollTopBtn from "./module/scroll-top-btn.js";
 import { swiper } from "./module/swiper.js";
 import { typed } from "./module/typed.js";
 
-hola
-
 document.addEventListener("DOMContentLoaded", e => {
     scrollTopBtn(".scroll-top")
     swiper,
-    typed
+    typed,
+    menuShow(".menu-btn", ".menu", ".menu-link a")
 })
